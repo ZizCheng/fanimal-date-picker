e597ec0d0b7bd9487bbaa945a2d6ada24a259fd5
Ziz Cheng <zizcheng@berkeley.edu>
2019-12-25 23:58:49 -0800

    readability revisions

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Initialize variables
 const now = new Date();
 var firstSelected = null;
 var secondSelected = null;
@@ -5,10 +6,6 @@ var monthOffset = 0;
 var currentMonth = now.getMonth();
 var currentYear = now.getFullYear();
 var calendar;
-
-// Initial Launch
-
-
 
 function getAllDaysInMonth(year, month){
   var days = []
@@ -24,97 +21,92 @@ function toggleCalendar() {
   calendar.hidden = !calendar.hidden;
 }
 
+function populateCalendar() {
+  //empty the current calendar
+  document.getElementById("calendarBody").innerHTML = "";
 
-function populateCalendar(y, m) {
-  calendarBody = WholeCalendar.shadowRoot.getElementById("calendarBody")
-  days = getAllDaysInMonth(y, m);
+  //save for later
+  calendarBody = document.getElementById("calendarBody");
+  days = getAllDaysInMonth(currentYear, currentMonth);
+
   // Fill days so that they are organized.
   fillEmptyDays = days[0].getDay()
   fillEmptyDays = fillEmptyDays < 0 ? 0 : fillEmptyDays
-  days = (new Array(fillEmptyDays).fill("-")).concat(days)
+  days = (new Array(fillEmptyDays).fill(null)).concat(days)
+
+  //population:
+  //per week/row:
   for(var row = 0; row < Math.ceil(days.length / 7); row++ ){
     currentRow = calendarBody.insertRow(row);
+
+    //per day/date:
     for(var day = 0; day < 7; day++){
 
-      if(day+(row*7) >= days.length){
-        // Ignore that for now.
-      }
-      else
-      {
+      //make sure still within month
+      if(!(day+(row*7) >= days.length)){
+        thisCell = currentRow.insertCell(day);
 
-          if(days[day+row*7] == "-"){
-            thisCell = currentRow.insertCell(day);
-            thisCell.textContent = ""
+        //null means should be empty
+        if(days[day+row*7] == null){
+          thisCell.textContent = "";
+        }
+        else
+        {
+          date = days[day+(row*7)]
+          thisCell.onclick = daySelected
+          thisCell.textContent = date.getDate();
+
+          if((firstSelected != null && firstSelected.getTime() == date.getTime()) || (secondSelected != null && secondSelected.getTime() == date.getTime())){
+            thisCell.style["background-color"] = "#00d1b2" //first/second selected color
           }
-          else
-          {
-            date = days[day+(row*7)]
-            thisCell = currentRow.insertCell(day);
-            thisCell.onclick = daySelected
-            thisCell.textContent = date.getDate();
-
-            if(firstSelected != null && firstSelected.getTime() == date.getTime()){
-              thisCell.style["background-color"] = "#00d1b2"
-            }
-            if(secondSelected != null && secondSelected.getTime() == date.getTime()){
-              thisCell.style["background-color"] = "#00d1b2"
-            }
-            if(secondSelected != null && firstSelected != null && date.getTime() > firstSelected.getTime() && date.getTime() < secondSelected.getTime()){
-              thisCell.style["background-color"] = "rgba(0, 209, 178, 0.35)"
-            }
-            if(date.getTime() < now.getTime()){
-              thisCell.style["background-color"] = "#dddddd";
-            }
-
+          if(secondSelected != null && firstSelected != null && date.getTime() > firstSelected.getTime() && date.getTime() < secondSelected.getTime()){
+            thisCell.style["background-color"] = "rgba(0, 209, 178, 0.35)" //in between colors
           }
+          if(date.getTime() < now.getTime()){
+            thisCell.style["background-color"] = "#dddddd"; //gray out days that have already passed
+          }
+        }
       }
-
     }
   }
-
+  //change the month name
+  document.getElementById("MonthLabel").textContent = new Date(currentYear, currentMonth).toLocaleDateString('en-us', { year: 'numeric', month: 'long'});
 }
-// calendar.hidden = true;
 
-
-
+function moveMonth(direction) {
+  //change initial variables for populateCalendar
+  monthOffset += direction;
+  currentYear = now.getFullYear() + (Math.floor((now.getMonth()+monthOffset)/12));
+  currentMonth = ((now.getMonth()+monthOffset) % 12);
+  populateCalendar();
+}
 function calendarForward() {
-  calendarBody = WholeCalendar.shadowRoot.getElementById("calendarBody")
-  CurrentDay = WholeCalendar.shadowRoot.getElementById("CurrentDay");
-  calendarBody.innerHTML = ""
-  monthOffset++;
-  desiredYear = now.getFullYear() + (Math.floor((now.getMonth()+monthOffset)/12));
-  desiredMonth = ((now.getMonth()+monthOffset) % 12);
-  populateCalendar(desiredYear, desiredMonth);
-  currentYear = desiredYear;
-  currentMonth = desiredMonth;
-  CurrentDay.textContent = new Date(desiredYear, desiredMonth).toLocaleDateString('en-us', { year: 'numeric', month: 'long'})
+  moveMonth(1);
 }
-
 function calendarBackwards() {
-  calendarBody = WholeCalendar.shadowRoot.getElementById("calendarBody")
-  CurrentDay = WholeCalendar.shadowRoot.getElementById("CurrentDay");
-  calendarBody.innerHTML = ""
-  monthOffset--;
-  desiredYear = now.getFullYear() + (Math.floor((now.getMonth()+monthOffset)/12));
-  desiredMonth = ((now.getMonth()+monthOffset) % 12);
-  populateCalendar(desiredYear, desiredMonth);
-  currentYear = desiredYear;
-  currentMonth = desiredMonth;
-  CurrentDay.textContent = new Date(desiredYear, desiredMonth).toLocaleDateString('en-us', { year: 'numeric', month: 'long'})
+  moveMonth(-1);
 }
 
 function daySelected(ev){
-  calendarBody = WholeCalendar.shadowRoot.getElementById("calendarBody")
+  //save for later
+  calendarBody = document.getElementById("calendarBody")
   selectedDay = parseInt(ev.srcElement.textContent)
   selectedDate = new Date(currentYear, currentMonth, selectedDay)
+
+  //if selected time is in past
   if(selectedDate.getTime() < now.getTime()){
     return;
   }
 
+  //nothing selected as first
   if(firstSelected == null){
     firstSelected = selectedDate
   }
+
+  //something selected as first
   else if(secondSelected == null){
+
+    //if second selected is before first selected
     if(firstSelected.getTime() > selectedDate.getTime()){
       firstSelected = selectedDate
       secondSelected = null
@@ -125,33 +117,31 @@ function daySelected(ev){
       alert("dates chosen are: " + firstSelected + " until " + secondSelected)
     }
   }
+
+  //if both first and second selected, start as new
   else {
     firstSelected = selectedDate
     secondSelected = null;
   }
-
-  calendarBody.innerHTML = ""
-  populateCalendar(currentYear, currentMonth)
+  populateCalendar()
 }
 
+//tag stuff:
 class Calendar extends HTMLElement {
 
-  constructor() {
-    super();
-    const template = document.createElement('template');
-    template.innerHTML = this.render();
-    const shadowRoot = this.attachShadow({ mode: 'open' });
-    shadowRoot.appendChild(template.content.cloneNode(true));
-
+  //called when element is added into HTML
+  connectedCallback() {
+    this.innerHTML = this.render();
+    populateCalendar()
   }
-
+  //the HTML part
   render(){
     return `<div class="datepicker" id="calendar" style="position: fixed;">
           <div class="">
               <div class="container" style="position: relative; display: block;">
                   <div class="columns is-vcentered">
                     <div class="column is-one-fifth"><button onclick="calendarBackwards()" class="button"><i class="fas fa-arrow-left">backwards</i></button></div>
-                    <div class="column"> <h6 class="title is-6 has-text-black has-text-centered" id="CurrentDay">---</h6> </div>
+                    <div class="column"> <h6 class="title is-6 has-text-black has-text-centered" id="MonthLabel">---</h6> </div>
                     <div class="column is-one-fifth" ><button class="button" onclick="calendarForward()"><i class="fas fa-arrow-right">forward</i></button></div>
                   </div>
                 <table class="table is-bordered">
@@ -173,12 +163,5 @@ class Calendar extends HTMLElement {
   }
 }
 
+//define the element
 customElements.define('fanimal-calendar', Calendar);
-
-
-window.onload = function() {
-  calendar = WholeCalendar.shadowRoot.getElementById("calendar");
-  CurrentDay = WholeCalendar.shadowRoot.getElementById("CurrentDay");
-  populateCalendar(now.getFullYear(), now.getMonth())
-  CurrentDay.textContent = now.toLocaleDateString('en-us', { year: 'numeric', month: 'long'})
-}
